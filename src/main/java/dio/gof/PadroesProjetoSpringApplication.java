6063a76797795e88bb3bfad6b0c7088cd9ff1cff
Nickolas Silva <77502906+NaSilvaNick@users.noreply.github.com>
2022-11-07 19:56:33 -0300

    Projeto finalizado

--- src/main/java/dio/gof/PadroesProjetoSpringApplication.java
+++ src/main/java/dio/gof/PadroesProjetoSpringApplication.java
@@ -3,18 +3,7 @@ package dio.gof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Projeto Spring Boot gerado via Spring Initializer.
- * Os seguintes modulos foram selecionados:
- * - Spring Data JPA
- * - Spring Web
- * - H2 Database
- * - OpenFeing
- *
- * @author nasilvanick
- */
 @SpringBootApplication
 @EnableFeignClients
 public class PadroesProjetoSpringApplication {
--- src/main/java/dio/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/dio/gof/service/impl/ClienteServiceImpl.java
@@ -1,38 +1,66 @@
 package dio.gof.service.impl;
 
 import dio.gof.model.Cliente;
+import dio.gof.model.Endereco;
+import dio.gof.repository.ClienteRepository;
+import dio.gof.repository.EnderecoRepository;
 import dio.gof.service.ClienteService;
+import dio.gof.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
-  // TODO Singleton: Injetar os componentes do Spring com @Autowired.
-  // TODO Strategy: Implementar os métodos definidos na interface.
-  // TODO Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+  @Autowired
+  private ClienteRepository clienteRepository;
+  @Autowired
+  private EnderecoRepository enderecoRepository;
+  @Autowired
+  private ViaCepService viaCepService;
 
   @Override
   public Iterable<Cliente> buscarTodos() {
-    return null;
+    return clienteRepository.findAll();
   }
 
   @Override
   public Cliente buscarPorId(Long id) {
-    return null;
+    Optional<Cliente> clienteOp = clienteRepository.findById(id);
+    return clienteOp.get();
+  }
+
+  private void salvarClienteComCep(Cliente cliente) {
+    String cep = cliente.getEndereco().getCep();
+    Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+      Endereco novoEndereco = viaCepService.consultarCep(cep);
+      enderecoRepository.save(novoEndereco);
+      return novoEndereco;
+    });
+    cliente.setEndereco(endereco);
+    clienteRepository.save(cliente);
   }
 
   @Override
   public void inserir(Cliente cliente) {
-
+    salvarClienteComCep(cliente);
   }
 
   @Override
   public void atualizar(Long id, Cliente cliente) {
-
+    Optional<Cliente> clienteOp = clienteRepository.findById(id);
+    if (clienteOp.isPresent()) {
+      salvarClienteComCep(cliente);
+    }
   }
 
   @Override
   public void deletar(Long id) {
-
+    Optional<Cliente> clienteOp = clienteRepository.findById(id);
+    if (clienteOp.isPresent()) {
+      clienteRepository.deleteById(id);
+    }
   }
 }
